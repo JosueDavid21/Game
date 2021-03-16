e6393351c268822bdebe82c2e3835a56cb52c3e7
D-ave-code <79019118+D-ave-code@users.noreply.github.com>
2021-03-16 00:10:55 -0500

    modificque y agregue las islas

--- src/listas/ListaMapas.java
+++ src/listas/ListaMapas.java
@@ -33,6 +33,10 @@ public class ListaMapas {
         lista.put("amor", new Isla("amor", getRutaIMG()+"amor.png", new GenerarIsla(getRutaJSON().concat("amor.json")).obtenerMatriz()));
         lista.put("calavera", new Isla("calavera", getRutaIMG()+"calavera.png", new GenerarIsla(getRutaJSON().concat("calavera.json")).obtenerMatriz()));
         lista.put("volcan", new Isla("volcan", getRutaIMG()+"volcan.png", new GenerarIsla(getRutaJSON().concat("volcan.json")).obtenerMatriz()));
+        lista.put("playa", new Isla("playa", getRutaIMG()+"playa.png", new GenerarIsla(getRutaJSON().concat("playa.json")).obtenerMatriz()));
+        lista.put("selva", new Isla("selva", getRutaIMG()+"selva.png", new GenerarIsla(getRutaJSON().concat("selva.json")).obtenerMatriz()));
+        lista.put("circo", new Isla("circo", getRutaIMG()+"circo.png", new GenerarIsla(getRutaJSON().concat("circo.json")).obtenerMatriz()));
+        lista.put("dulce", new Isla("dulce", getRutaIMG()+"dulce.png", new GenerarIsla(getRutaJSON().concat("dulce.json")).obtenerMatriz()));
 
         
         
