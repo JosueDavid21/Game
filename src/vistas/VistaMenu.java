8ac95bed4a1318bac164095d11353c9322b26ccc
edgar <edgar@DESKTOP-B5JRV3M>
2021-03-17 13:24:23 -0500

    agragar clases

--- src/vistas/VistaMenu.java
+++ src/vistas/VistaMenu.java
@@ -10,44 +10,43 @@ import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+
 
 /**
  *
  * @author grace
  */
 public class VistaMenu extends javax.swing.JDialog {
- barra musica = new barra();
-    String nombresonido ="src/musica/juego-de-tronos-2.wav";
+    Reproducir des = new Reproducir();
+    String nombresonido = "src/musica/juego-de-tronos-2.wav";
     Clip clip = null;
-      
-      
+
     public VistaMenu(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
-     
-         initComponents();
-         
-         ImageIcon de = new ImageIcon("C:\\Users\\edgar\\Documents\\NetBeansProjects\\Game\\src\\imagenes\\ayuda\\menu.gif");
-         Icon ide = new ImageIcon(de.getImage().getScaledInstance(Jgiff.getWidth(), Jgiff.getHeight(), Image.SCALE_DEFAULT));
-         Jgiff.setIcon(ide);
-         ImageIcon d = new ImageIcon("C:\\Users\\edgar\\Documents\\NetBeansProjects\\Game\\src\\imagenes\\ayuda\\j_1.png");
-        Icon id = new ImageIcon(d.getImage().getScaledInstance(lb3.getWidth(), lb3.getHeight(), Image.SCALE_DEFAULT));
-        lb3.setIcon(id);
-         ImageIcon dee = new ImageIcon("C:\\Users\\edgar\\Documents\\NetBeansProjects\\Game\\src\\imagenes\\ayuda\\sin.png");
-        Icon idd = new ImageIcon(dee.getImage().getScaledInstance(lb4.getWidth(), lb4.getHeight(), Image.SCALE_DEFAULT));
-        lb4.setIcon(idd);
-         this.repaint();
-         this.setLocationRelativeTo(this);
-               
+
+        initComponents();
+
+        ImageIcon de = new ImageIcon("C:\\Users\\edgar\\Documents\\NetBeansProjects\\Game\\src\\imagenes\\ayuda\\menu.gif");
+        Icon ide = new ImageIcon(de.getImage().getScaledInstance(Fondo.getWidth(), Fondo.getHeight(), Image.SCALE_DEFAULT));
+        Fondo.setIcon(ide);
+        ImageIcon d = new ImageIcon("C:\\Users\\edgar\\Documents\\NetBeansProjects\\Game\\src\\imagenes\\ayuda\\j_1.png");
+        Icon id = new ImageIcon(d.getImage().getScaledInstance(play.getWidth(), play.getHeight(), Image.SCALE_DEFAULT));
+        play.setIcon(id);
+        ImageIcon dee = new ImageIcon("C:\\Users\\edgar\\Documents\\NetBeansProjects\\Game\\src\\imagenes\\ayuda\\sin.png");
+        Icon idd = new ImageIcon(dee.getImage().getScaledInstance(pause.getWidth(), pause.getHeight(), Image.SCALE_DEFAULT));
+        pause.setIcon(idd);
+        this.repaint();
+        this.setLocationRelativeTo(this);
+        
+        try {
+            des.ReproducirSonido(nombresonido);
+        } catch (Exception e) {
+        }
+
     }
-       
-    
 
     VistaMenu() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -63,10 +62,11 @@ public class VistaMenu extends javax.swing.JDialog {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        lb3 = new javax.swing.JLabel();
-        lb4 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        Jgiff = new javax.swing.JLabel();
+        play = new javax.swing.JLabel();
+        pause = new javax.swing.JLabel();
+        iniciojuego = new javax.swing.JLabel();
+        salir = new javax.swing.JLabel();
+        Fondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setUndecorated(true);
@@ -75,104 +75,65 @@ public class VistaMenu extends javax.swing.JDialog {
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
-        lb3.setBackground(new java.awt.Color(51, 51, 255));
-        lb3.setFont(new java.awt.Font("Yu Gothic UI Light", 3, 18)); // NOI18N
-        lb3.setForeground(new java.awt.Color(255, 255, 255));
-        lb3.setText("Mute");
-        lb3.addMouseListener(new java.awt.event.MouseAdapter() {
+        play.setBackground(new java.awt.Color(51, 51, 255));
+        play.setFont(new java.awt.Font("Yu Gothic UI Light", 3, 18)); // NOI18N
+        play.setForeground(new java.awt.Color(255, 255, 255));
+        play.setText("Mute");
+        play.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                lb3MouseClicked(evt);
+                playMouseClicked(evt);
             }
         });
-        getContentPane().add(lb3, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 10, 50, 40));
+        getContentPane().add(play, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 10, 20, 20));
 
-        lb4.addMouseListener(new java.awt.event.MouseAdapter() {
+        pause.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                lb4MouseClicked(evt);
+                pauseMouseClicked(evt);
             }
         });
-        getContentPane().add(lb4, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 60, 50, 40));
+        getContentPane().add(pause, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 10, 20, 20));
 
-        jLabel2.setFont(new java.awt.Font("Segoe UI Semilight", 0, 24)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel2.setText("        Iniciar Juego");
-        jLabel2.addMouseListener(new java.awt.event.MouseAdapter() {
+        iniciojuego.setFont(new java.awt.Font("Segoe UI Semilight", 0, 24)); // NOI18N
+        iniciojuego.setForeground(new java.awt.Color(255, 255, 255));
+        iniciojuego.setText("        Iniciar Juego");
+        iniciojuego.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jLabel2MouseClicked(evt);
+                iniciojuegoMouseClicked(evt);
             }
         });
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 150, 230, 50));
-        getContentPane().add(Jgiff, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 360, 470));
+        getContentPane().add(iniciojuego, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 150, 230, 50));
+
+        salir.setText("Salir");
+        salir.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                salirMouseClicked(evt);
+            }
+        });
+        getContentPane().add(salir, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 280, 30, 20));
+        getContentPane().add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 360, 310));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void lb3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lb3MouseClicked
-            try {                                     
-                clip.stop();
-               
-                
-                try {
-                    clip = AudioSystem.getClip();
-                } catch (LineUnavailableException ex) {
-                    Logger.getLogger(VistaMenu.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                
-                AudioInputStream audioInputStream =null;
-                try {
-                    try {
-                        audioInputStream = AudioSystem.getAudioInputStream(new File(nombresonido).getAbsoluteFile());
-                    } catch (IOException ex) {
-                        Logger.getLogger(VistaMenu.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                } catch (UnsupportedAudioFileException ex) {
-                    Logger.getLogger(VistaInicial.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                
-                clip.open(audioInputStream);
-                clip.start();
-            } catch (LineUnavailableException ex) {
-                Logger.getLogger(VistaMenu.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (IOException ex) {
-         Logger.getLogger(VistaMenu.class.getName()).log(Level.SEVERE, null, ex);
-     }
-        
-    }//GEN-LAST:event_lb3MouseClicked
+    private void playMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_playMouseClicked
+        try {
+           des.ReproducirSonido(nombresonido); 
+        } catch (Exception e) {
+        }
 
-    private void jLabel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseClicked
-       
-    }//GEN-LAST:event_jLabel2MouseClicked
+    }//GEN-LAST:event_playMouseClicked
 
-    private void lb4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lb4MouseClicked
-      try {                                     
-                clip.stop();
-                String nombrs ="src/musica/haga-clic-en_4.wav";
-                
-                try {
-                    clip = AudioSystem.getClip();
-                } catch (LineUnavailableException ex) {
-                    Logger.getLogger(VistaMenu.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                
-                AudioInputStream audioInputStream =null;
-                try {
-                    try {
-                        audioInputStream = AudioSystem.getAudioInputStream(new File(nombrs).getAbsoluteFile());
-                    } catch (IOException ex) {
-                        Logger.getLogger(VistaMenu.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                } catch (UnsupportedAudioFileException ex) {
-                    Logger.getLogger(VistaInicial.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                
-                clip.open(audioInputStream);
-                clip.start();
-            } catch (LineUnavailableException ex) {
-                Logger.getLogger(VistaMenu.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (IOException ex) {
-         Logger.getLogger(VistaMenu.class.getName()).log(Level.SEVERE, null, ex);
-     }
-    }//GEN-LAST:event_lb4MouseClicked
+    private void iniciojuegoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_iniciojuegoMouseClicked
+
+    }//GEN-LAST:event_iniciojuegoMouseClicked
+
+    private void pauseMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pauseMouseClicked
+       des.Pausa();
+    }//GEN-LAST:event_pauseMouseClicked
+
+    private void salirMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_salirMouseClicked
+    System.exit(0);
+    }//GEN-LAST:event_salirMouseClicked
 
     /**
      * @param args the command line arguments
@@ -217,10 +178,11 @@ public class VistaMenu extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel Jgiff;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel Fondo;
+    private javax.swing.JLabel iniciojuego;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JLabel lb3;
-    private javax.swing.JLabel lb4;
+    private javax.swing.JLabel pause;
+    private javax.swing.JLabel play;
+    private javax.swing.JLabel salir;
     // End of variables declaration//GEN-END:variables
 }
